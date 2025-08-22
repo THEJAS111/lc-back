b0b53d207522a708903797bd278b75a3b27c2577
Thejas <tejasshetty189@gmail.com>
2025-08-22 21:49:22 +0530

    addding vercel.json

---
+++ leetcode/src/config/db.js
@@ -0,0 +1,7 @@
+const mongoose =require('mongoose');
+
+async function main(){
+    mongoose.connect(process.env.DB_CONNECT_STRING);
+    console.log("connected")
+}
+module.exports=main;
--- leetcodefront/frontend/src/utils/axiosclient.js
+++ leetcodefront/frontend/src/utils/axiosclient.js
@@ -1,11 +1,10 @@
 import axios from "axios"
 
-const axiosClient = axios.create({
-  baseURL: isDev
-    ? "http://localhost:3000" // local backend when developing
-    : "https://your-backend.vercel.app", // change this to your deployed backend
-  withCredentials: true,
-  headers: { "Content-Type": "application/json" },
+const axiosClient= axios.create({
+  baseURL: "http://localhost:3000",
+  withCredentials:true,
+  headers: { 'Content-Type':'application/json'
+  }
 });
 
 export default axiosClient;
