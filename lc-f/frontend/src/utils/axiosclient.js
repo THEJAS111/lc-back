b50d372ecc20f16c6852b042e24d8af28a602f9b
Thejas <tejasshetty189@gmail.com>
2025-08-23 14:38:01 +0530

    changed axios

--- lc-f/frontend/src/utils/axiosclient.js
+++ lc-f/frontend/src/utils/axiosclient.js
@@ -1,10 +1,10 @@
 import axios from "axios"
 
-const axiosClient= axios.create({
-  baseURL: "http://localhost:3000",
-  withCredentials:true,
-  headers: { 'Content-Type':'application/json'
-  }
+const axiosClient = axios.create({
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://lc-back-ny9k.vercel.app/",
+  withCredentials: true,
+  headers: { "Content-Type": "application/json" },
 });
 
 export default axiosClient;
