177d0a0eef19593d773c469e8427caca665bf3b4
THEJAS111 <tejasshetty189@gmail.com>
2025-11-27 00:00:27 +0530

    Update redis.js

--- leetcode/src/config/redis.js
+++ leetcode/src/config/redis.js
@@ -1,10 +1,10 @@
 const{ createClient }=require('redis');
 const redisclient = createClient({
-  username: "default",
-  password: process.env.REDIS_PASS,
-  socket: {
-    host: "redis-19901.c74.us-east-1-4.ec2.redns.redis-cloud.com",
-    port: 19901,
-  },
+    username:"default",
+    password: process.env.REDIS_PASS,
+    socket: {
+        host: 'redis-17693.c98.us-east-1-4.ec2.cloud.redislabs.com',
+        port: 17693
+    }
 });
 module.exports=redisclient;
